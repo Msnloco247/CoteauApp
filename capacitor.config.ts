4ceaa08a082757254e06d97d1c6d1bad16a4f8fd
Abner <msnloco247@hotmail.com>
2024-06-24 23:43:12 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'coteau',
+  webDir: 'dist'
+};
+
+export default config;
